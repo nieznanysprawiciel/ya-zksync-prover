eb5af5ee3f981958c55f0ed6910f2c3987cecbba
nieznany.sprawiciel <witek@golem.network>
2020-11-05 23:56:09 +0100

    zksync server communication on requestor side - first attempts

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use anyhow::anyhow;
 use chrono::Utc;
 use futures::prelude::*;
 use std::ops::Add;
 use structopt::StructOpt;
 
+use std::time::Duration;
+use url::Url;
 use ya_agreement_utils::{constraints, ConstraintKey, Constraints};
 use ya_client::web::WebClient;
 use yarapi::requestor::Image;
 use yarapi::rest::{self, Activity};
+use zksync_prover::{client as zksync, ApiClient};
 
 const PACKAGE: &str = "{TODO package}";
 
@@ -58,6 +62,8 @@ struct Args {
     subnet: String,
     #[structopt(long, env = "YAGNA_APPKEY")]
     appkey: String,
+    #[structopt(long, env)]
+    server_api_url: String,
 }
 
 #[actix_rt::main]
@@ -65,6 +71,10 @@ pub async fn main() -> anyhow::Result<()> {
     let args = Args::from_args();
     std::env::set_var("RUST_LOG", "info,yarapi::drop=debug");
     env_logger::init();
+
+    let server_api_url: Url = args.server_api_url.parse()?;
+    let zksync_client =
+        zksync::ApiClient::new(&server_api_url, "yagna-node", Duration::from_secs(69));
 
     let client = WebClient::with_token(&args.appkey);
     let session = rest::Session::with_client(client.clone());
@@ -74,11 +84,32 @@ pub async fn main() -> anyhow::Result<()> {
             let agreement = create_agreement(session.market()?, &args.subnet).await?;
             let activity = session.create_activity(&agreement).await?;
 
-            // TODO: run zksync here
+            let prover_id = zksync_client.register_prover(0)?;
 
+            // TODO: run zksync in loop here
+            prove_block(zksync_client.clone()).await?;
+
+            zksync_client.prover_stopped(prover_id)?;
             activity.destroy().await?;
+
             Ok::<_, anyhow::Error>(())
         })
         .await
         .unwrap_or_else(|| anyhow::bail!("ctrl-c caught"))
+}
+
+async fn prove_block(zksync_client: zksync::ApiClient) -> anyhow::Result<()> {
+    // TODO: Set block size
+    let block_size = 0;
+
+    let (block_id, job_id) = zksync_client
+        .block_to_prove(block_size)
+        .map_err(|e| anyhow!("Failed to download block to prove. Error: {}", e))?
+        .ok_or(anyhow!("Failed to find block of size {}", block_size))?;
+
+    let instance = zksync_client
+        .prover_data(block_id)
+        .map_err(|e| anyhow!("Couldn't get data for block '{}'. Error: {}", block_id, e))?;
+
+    Ok(())
 }
