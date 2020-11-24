c5c4e246259b67d6f13a34bdeffa4777b7ea73e9
nieznany.sprawiciel <witek@golem.network>
2020-11-24 22:42:17 +0100

    Progressing with moving files from zksync branch

--- docker/build-dockers.sh
+++ docker/build-dockers.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-cd docker/geth
 docker build -t my-geth:0.2 -f docker/geth/Dockerfile .
-
-cd ../..
 docker build -t ya-zksync-prover:0.2 -f docker/prover/Dockerfile .
--- docker/run-zksync-server.sh
+++ docker/run-zksync-server.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+mkdir -p volumes
+mkdir -p volumes/geth
+mkdir -p volumes/postgres
+mkdir -p volumes/tesseracts
+
+USER_ID="$(id -u)"
+GROUP_ID="$(id -g)"
+export CURRENT_UID="${USER_ID}:${GROUP_ID}"
+
+echo "Runnig as user with ids: ${CURRENT_UID}"
+docker-compose up -d
+
